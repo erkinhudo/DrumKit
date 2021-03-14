f62cb319d0516eb27eb61504c718ed94ccb8a243
Eric Hudo <erkinhudo@gmail.com>
2021-03-13 23:30:47 -0500

    Sounds added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 for (var i=0; i<document.querySelectorAll(".drum").length; i++){
 
 document.querySelectorAll(".drum")[i].addEventListener("click",function(){
-    alert("I got clicked!");
+    var audio = new Audio("sounds/tom-1.mp3");
+    audio.play();;
 });}
 
