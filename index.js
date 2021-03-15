6003137245912bb3f08fe418c19e2a7e3a11b806
Eric Hudo <erkinhudo@gmail.com>
2021-03-14 21:54:30 -0400

    Voices was added

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+// var sound = ["crash.png","kick.png","snare.png","tom1.png","tom2.png","tom3.png","tom4.png"]
 
 for (var i=0; i<document.querySelectorAll(".drum").length; i++){
 
+  
 document.querySelectorAll(".drum")[i].addEventListener("click",function(){
-    var audio = new Audio("sounds/tom-1.mp3");
-    audio.play();;
+   console.log(this.style.color="white");
 });}
 
+// var audio = new Audio("sounds/sound[i]");
+// audio.play();;
