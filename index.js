28da22ead8dd87ed5ad378f5fce3090805403aca
Eric Hudo <erkinhudo@gmail.com>
2021-03-15 00:02:00 -0400

    All done

--- index.js
+++ index.js
@@ -1,11 +1,65 @@
-// var sound = ["crash.png","kick.png","snare.png","tom1.png","tom2.png","tom3.png","tom4.png"]
+// Detecting Button Press
 
 for (var i=0; i<document.querySelectorAll(".drum").length; i++){
 
   
 document.querySelectorAll(".drum")[i].addEventListener("click",function(){
-   console.log(this.style.color="white");
-});}
+     
+   var buttonInnerHTML = this.innerHTML;
+ 
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+   });
+}
 
-// var audio = new Audio("sounds/sound[i]");
-// audio.play();;
+// Detecting Keyboard press
+
+document.addEventListener("keypress", function (event){
+   makeSound(event.key);
+   buttonAnimation(event.key);
+});
+
+function makeSound (key){
+   
+   switch (key){
+      case "w":
+        var audio = new Audio("sounds/tom-1.mp3");
+        audio.play();
+      break;
+      case "a":
+         var audio = new Audio ("sounds/tom-2.mp3");
+         audio.play();
+      break;
+      case "s":
+        var audio = new Audio ("sounds/tom-3.mp3");
+        audio.play();
+     break;   
+     case "d":
+        var audio = new Audio ("sounds/tom-4.mp3");
+        audio.play();
+     break;
+     case "j":
+        var audio = new Audio ("sounds/snare.mp3");
+        audio.play();
+     break;
+     case "k":
+        var audio = new Audio ("sounds/crash.mp3");
+        audio.play();
+     break;
+     case "l":
+        var audio = new Audio ("sounds/kick-bass.mp3")
+        audio.play();
+     break;
+
+     default: console.log(buttonInnerHTML);
+   }
+}
+
+
+function buttonAnimation (currentKey){
+   var activeButton =  document.querySelector("."+currentKey);
+   activeButton.classList.add("pressed");
+
+   setTimeout(function(){
+      activeButton.classList.remove("pressed");},100); 
+   }
